63e596076e42fe9d2f3629c9860534ed505275c2
omodtung <dothedung131@gmail.com>
2024-07-18 12:54:09 +0700

    Ui for Register Site

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,18 @@ import Footer from "./components/Footer";
 import Home from "./components/Home";
 import contactPage from "./pages/contact";
 import bookPage from "./pages/book";
+import RegisterPage from "./pages/register";
 const Layout = () => {
-  return <div className="layout-app">
-<Header></Header>
-<Outlet></Outlet>
-<Footer></Footer>
-  </div>;
+  return (
+    <div className="layout-app">
+      <Header></Header>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
+// dothedung131
+// eye6pipl23iGDK1E
 function App() {
   const router = createBrowserRouter([
     {
@@ -22,26 +27,29 @@ function App() {
 
       element: <Layout />,
       errorElement: <div> 404 not found h</div>,
-      children :
-      [   // @ts-ignore
-        {index : true ,element : <Home></Home>},
+      children: [
+        // @ts-ignore
+        { index: true, element: <Home></Home> },
         {
-          path : "contact",
-             // @ts-ignore
-          element :<contactPage></contactPage>
-        }
-        ,
-        {
-          path : "book",
-             // @ts-ignore
-          element : <bookPage></bookPage>
+          path: "contact",
+          // @ts-ignore
+          element: <contactPage></contactPage>,
         },
-
+        {
+          path: "book",
+          // @ts-ignore
+          element: <bookPage></bookPage>,
+        },
       ],
     },
     {
       path: "/login",
       element: <LoginPage />,
+    },
+    {
+      path: "/register",
+
+      element: <RegisterPage></RegisterPage>,
     },
   ]);
   return (
--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,4 +1,67 @@
-const register = () => {
-  return <> register</>;
+import type { FormProps } from "antd";
+import { Button, Checkbox, Form, Input } from "antd";
+import { Divider } from "antd";
+const RegisterPage = () => {
+
+  type FieldType = {
+    username?: string;
+    password?: string;
+    remember?: string;
+  };
+  
+  const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+    console.log('Success:', values);
+  };
+  
+  const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+  
+  return (
+    <div className="register-page" style={{ padding: "50px" }}>
+      <h3 style={{ textAlign: "center" }}> Dang ki nguoi dung moi</h3>
+      <Divider></Divider>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{ offset: 8, span: 16 }}
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 };
-export default register;
+export default RegisterPage;
