c2c35dcdd4b58aaa6596c759853fae635e255a41
omodtung <dothedung131@gmail.com>
2024-07-19 22:09:56 +0700

    session about for porfolio

--- src/components/content/about.tsx
+++ src/components/content/about.tsx
@@ -1,135 +1,143 @@
-
-import  aboutDev from '../../assets/img/about/macos.jpg'
-import myCv from '../../assets/cv.pdf'
-const About=() =>
-{
-    return(
-        <>
-            <div className="arlo_tm_section relative" id="about">
-                <div className="arlo_tm_about_wrapper_all">
-                    <div className="container">
-                        <div className="arlo_tm_title_holder">
-                            <h3>About Me</h3>
-                            <span>Main informations about me</span>
-                        </div>
-                        <div className="arlo_tm_about_wrap">
-                            <div className="author_wrap">
-                                <div className="leftbox">
-                                    <div
-                                        className="about_image_wrap parallax"
-                                        data-relative-input="true"
-                                    >
-                                        <div className="image layer" data-depth="0.1">
-                                            <img src="img/about/550x640.jpg" alt="550x640"/>
-                                            <div
-                                                className="inner"
-                                                data-img-url={aboutDev}
-                                            ></div>
-                                        </div>
-                                        <div className="border layer" data-depth="0.2">
-                                            <img src="img/about/550x640.jpg" alt="550x640"/>
-                                            <div className="inner"></div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="rightbox">
-                                    <div className="arlo_tm_mini_title_holder">
-                                        <h4>
-                                            I'm Alan Michaelis and{" "}
-                                            <span className="arlo_tm_animation_text_word"></span>
-                                        </h4>
-                                    </div>
-                                    <div className="definition">
-                                        <p>
-                                            Hi! My name is <strong>Alan Michaelis</strong>. I am
-                                            a Web Developer, and I'm very passionate and
-                                            dedicated to my work. With 20 years experience as a
-                                            professional Web developer, I have acquired the
-                                            skills and knowledge necessary to make your project
-                                            a success. I enjoy every step of the design process,
-                                            from discussion and collaboration to concept and
-                                            execution, but I find the most satisfaction in
-                                            seeing the finished product do everything for you
-                                            that it was created to do.
-                                        </p>
-                                    </div>
-                                    <div className="about_short_contact_wrap">
-                                        <ul>
-                                            <li>
-                              <span>
-                                <label>Birthday:</label> 01.07.1990
-                              </span>
-                                            </li>
-                                            <li>
-                              <span>
-                                <label>Age:</label> 28
-                              </span>
-                                            </li>
-                                            <li>
-                              <span>
-                                <label>City:</label> New York, USA
-                              </span>
-                                            </li>
-                                            <li>
-                              <span>
-                                <label>Interests:</label> Soccer, UFC
-                              </span>
-                                            </li>
-                                            <li>
-                              <span>
-                                <label>Study:</label> Chicago University
-                              </span>
-                                            </li>
-                                            <li>
-                              <span>
-                                <label>Degree:</label> Master
-                              </span>
-                                            </li>
-                                            <li>
-                              <span>
-                                <label>Website:</label>{" "}
-                                  <a href="#">www.mywebsite.com</a>
-                              </span>
-                                            </li>
-                                            <li>
-                              <span>
-                                <label>Mail:</label>{" "}
-                                  <a href="mailto:example@gmail.com">
-                                  mymail&#64;gmail.com
-                                </a>
-                              </span>
-                                            </li>
-                                            <li>
-                              <span>
-                                <label>Phone:</label>{" "}
-                                  <a href="#">+77 022 177 05 05</a>
-                              </span>
-                                            </li>
-                                            <li>
-                              <span>
-                                <label>Twitter:</label>{" "}
-                                  <a href="#">&#64;myusername</a>
-                              </span>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                    <div className="buttons_wrap">
-                                        <ul>
-                                            <li>
-                                                <a href={myCv} download>
-                                                    <span>Download CV</span>
-                                                </a>
-                                            </li>
-
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+import aboutDev from "../../assets/img/about/macos.jpg";
+import myCv from "../../assets/cv.pdf";
+import { TypeAnimation } from "react-type-animation";
+const About = () => {
+  return (
+    <>
+      <div className="arlo_tm_section relative" id="about">
+        <div className="arlo_tm_about_wrapper_all">
+          <div className="container">
+            <div className="arlo_tm_title_holder">
+              <h3>About Me</h3>
+              <span>Main informations about me</span>
             </div>
-        </>
-    )
-}
+            <div className="arlo_tm_about_wrap">
+              <div className="author_wrap">
+                <div className="leftbox">
+                  <div
+                    className="about_image_wrap parallax"
+                    data-relative-input="true"
+                  >
+                    <div className="image layer" data-depth="0.1">
+                      <img src="img/about/550x640.jpg" alt="550x640" />
+                      <div className="inner" data-img-url={aboutDev}></div>
+                    </div>
+                    <div className="border layer" data-depth="0.2">
+                      <img src="img/about/550x640.jpg" alt="550x640" />
+                      <div className="inner"></div>
+                    </div>
+                  </div>
+                </div>
+                <div className="rightbox">
+                  <div className="arlo_tm_mini_title_holder">
+                    <h4>
+                      I'm The Tung and{" "}
+                      <TypeAnimation
+                        sequence={[
+                          // Same substring at the start will only be typed out once, initially
+                          "React Developer",
+                          1000, // wait 1s before replacing "Mice" with "Hamsters"
+                          "Nodejs Backend Developer",
+                          1000,
+                          "Next.js Developer",
+                          1000,
+                          "Java Backend Developer",
+                          1000,
+                        ]}
+                        wrapper="span"
+                        speed={50}
+                        repeat={Infinity}
+                      />
+                      {/*<span className="arlo_tm_animation_text_word"></span>*/}
+                    </h4>
+                  </div>
+                  <div className="definition">
+                    <p>
+                      Hi! Tôi tên là <strong> Thế Tùng </strong>. Tôi làm
+                      Engineer, tôi có kinh nghiệm với lập trình Backend và
+                      Frontend như là một Software Development Engineer,
+                      <br></br>
+                      <br/>
+
+                      Tôi có thể  làm việc với TypeScript , Java , JavaScript ,PHP 
+                      
+                    </p>
+                  </div>
+                  <div className="about_short_contact_wrap">
+                    <ul>
+                      <li>
+                        <span>
+                          <label>Birthday:</label> XX.XX.XXXX
+                        </span>
+                      </li>
+                      <li>
+                        <span>
+                          <label>Giới Tính :</label> {""}Nam
+
+                        </span>
+                      </li>
+                      <li>
+                        <span>
+                          <label>City:</label> Thành Phố  HCM, Việt Nam
+                        </span>
+                      </li>
+                      <li>
+                        <span>
+                          <label>GitHub:</label>{""} 
+                          <a href="https://github.com/omodtung">
+                          https://github.com/omodtung
+                          </a>
+                        </span>
+                      </li>
+                      <li>
+                        <span>
+                          <label>Study:</label> Đại Học Sài Gòn
+                        </span>
+                      </li>
+                      <li>
+                        <span>
+                          <label>Bằng Cấp:</label> Kỹ Sư Công Nghệ Thông Tin
+                        </span>
+                      </li>
+                      <li>
+                        <span>
+                          <label>Chuyên Ngành:</label> Kỹ Thuật Phần Mềm
+                          
+                        </span>
+                      </li>
+                      <li>
+                        <span>
+                          <label>Mail:</label>{" "}
+                          <a href="dothedung131@gmail.com">
+                            dothedung131@gmail.com
+                          </a>
+                        </span>
+                      </li>
+                      <li>
+                        <span>
+                          <label>Phone:</label>{" "}
+                          <a href="#">0792324232</a>
+                        </span>
+                      </li>
+
+                    </ul>
+                  </div>
+                  <div className="buttons_wrap">
+                    <ul>
+                      <li>
+                        <a href={myCv} download>
+                          <span>Download CV</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 export default About;
