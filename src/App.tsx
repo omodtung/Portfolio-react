c2a95627860fd7d4dcb0d97a238d0521338112d5
omodtung <dothedung131@gmail.com>
2024-08-27 01:43:26 +0700

    Project SIte

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@
 import Portfolio from "./screens/portfolio";
 import './styles/plugins.scss';
 import './styles/style.scss';
-// dothedung131
-// eye6pipl23iGDK1E
 function App() {
   return (
     <>
--- src/components/content/Projects.tsx
+++ src/components/content/Projects.tsx
@@ -62,7 +62,7 @@ const Project = () => {
       image: <SiNestjs size={50} color={"#ff1100"} />,
       title: " Backend Job Recruitment ",
       shortDescription:
-        " backend written in NestJs framework , communicates information with user interface in recruitment page ,connect to mongodb database",
+        " backend written in NestJs framework  ,connect to mongodb database and  MongoDB, TypeScript, JWT ,Docker   ",
       detail: {
         description:
           "The project copies current recruitment sites such as IT Viec, TopDev...",
@@ -74,48 +74,13 @@ const Project = () => {
         github: "https://github.com/omodtung/job-recruitment.git", //link video Or github or Deploy
       },
     },
-    {
-      image: <FaLaravel size={50} color={"#ff1100"} />,
-      title: " Furniture shop ",
-      shortDescription:
-        " backend written in NestJs framework , communicates information with user interface in recruitment page ,connect to mongodb database",
-      detail: {
-        description:
-          "The project copies current recruitment sites such as IT Viec, TopDev...",
-        Frontend: "using React on the frontend",
-        Backend: "NestJs framework And TypeScript",
-        database: "mongodb",
-        member: 1,
-        role: "Backend Dev",
-        github: "https://github.com/omodtung/job-recruitment.git", //link video Or github or Deploy
-      },
-    },
-    {
-      image: [
-       <FaBootstrap  size={50} color={"#b51fb5"}/>,
 
-       <VscBlank  size={30}/>,
-       <FaPhp size={50} color={"#2b00ff"} />, 
-    ],
-      title: " Backend Job Recruitment ",
-      shortDescription:
-        " backend written in NestJs framework , communicates information with user interface in recruitment page ,connect to mongodb database",
-      detail: {
-        description:
-          "The project copies current recruitment sites such as IT Viec, TopDev...",
-        Frontend: "using React on the frontend",
-        Backend: "NestJs framework And TypeScript",
-        database: "mongodb",
-        member: 1,
-        role: "Backend Dev",
-        github: "https://github.com/omodtung/job-recruitment.git", //link video Or github or Deploy
-      },
-    },
+
     {
       image: <SiNestjs size={50} color={"#ff1100"} />,
       title: " Backend Blogs ",
       shortDescription:
-        " backend written in NestJs framework , communicates information with user interface in Blog  Admin page and Client Page ,connect to mysql database",
+        " backend written in  NestJs framework , TypeScript , Mysql ,Docker ,TypeORM",
       detail: {
         description:
           "backend handles communication with DashBoard admin interface in managing posts and users and Client Site reading posts",
@@ -127,43 +92,75 @@ const Project = () => {
         github: "https://github.com/omodtung/nestjs_BE_Blogs.git", //link video Or github or Deploy
       },
     },
+
+
+
+
+
+
+
+
+    {
+      image: <FaLaravel size={50} color={"#ff1100"} />,
+      title: " Furniture shop ",
+      shortDescription:
+        "FurnitureShop - Ecommerce using Laravel MVC, Blade Templates, Boostrap, JQuery, Tabler, MySQL and integrate VNPay Payment, OAuth with Google & Facebook.",
+      detail: {
+        description:
+          "FurnitureShop - Ecommerce using Laravel MVC, Blade Templates, Boostrap, JQuery, Tabler, MySQL and integrate VNPay Payment, OAuth with Google & Facebook.",
+        Frontend: "Blade Templates ",
+        Backend: "Laravel",
+        database: "Mysql",
+        member: 4,
+        role: "Backend Dev",
+        github: "https://github.com/omodtung/furniture-shop", //link video Or github or Deploy
+      },
+    },
+    {
+      image: [
+        <FaBootstrap size={50} color={"#b51fb5"} />,
+
+        <VscBlank size={30} />,
+        <FaPhp size={50} color={"#2b00ff"} />,
+      ],
+      title: "  Student Management Application ",
+      shortDescription:
+        " Student Management Application - Boostrap 5 , PHP ,MYSQL , XAMPP , AJAX, JQUERY",
+      detail: {
+        description:
+          "Đồ Án Công Nghệ Phần Mềm - Boostrap 5 , PHP ,MYSQL , XAMPP , AJAX, JQUERY",
+        Frontend: "Boostrap 5",
+        Backend: "PHP",
+        database: "MYSQL",
+        member: 4,
+        role: "PHP Dev,Boostrap Dev",
+        github: "https://github.com/omodtung/Quan_Ly_Hoc_Sinh.git", //link video Or github or Deploy
+      },
+    },
+ 
     {
       image: <SiSpringboot size={50} color={"#40ff00"} />,
-      title: " Backend Blogs ",
+      title: " Member management",
       shortDescription:
-        " backend written in NestJs framework , communicates information with user interface in Blog  Admin page and Client Page ,connect to mysql database",
+        " Member management, Equipment, and Violation Handling using SpringBoot, Thymeleaf, MySQL",
       detail: {
         description:
-          "backend handles communication with DashBoard admin interface in managing posts and users and Client Site reading posts",
+          "Member management, Equipment, and Violation Handling using SpringBoot Backend, Thymeleaf is a modern server-side Java template engine, MySQL",
 
-        Backend: "NestJs framework And TypeScript",
-        database: "MysqL",
-        member: 1,
-        role: "Backend Dev",
-        github: "https://github.com/omodtung/nestjs_BE_Blogs.git", //link video Or github or Deploy
+        Backend: "Spring Boot",
+        Frontend :"Thymeleaf ,Bootstrap ",
+        database: "MySQL",
+        member: 9,
+        role: "Backend Dev ,Frontend Dev",
+        github: "https://github.com/omodtung/Platform_SpringCore.git", //link video Or github or Deploy
       },
     },
-    {
-      image: <SiHibernate size={50} color={"#40ff00"} />,
-      title: " Backend Blogs ",
-      shortDescription:
-        " backend written in NestJs framework , communicates information with user interface in Blog  Admin page and Client Page ,connect to mysql database",
-      detail: {
-        description:
-          "backend handles communication with DashBoard admin interface in managing posts and users and Client Site reading posts",
-
-        Backend: "NestJs framework And TypeScript",
-        database: "MysqL",
-        member: 1,
-        role: "Backend Dev",
-        github: "https://github.com/omodtung/nestjs_BE_Blogs.git", //link video Or github or Deploy
-      },
-    },
+   
     {
       image: <FaReact size={50} color={"#7BEDFA"} />,
       title: "Admin Blog Site",
       shortDescription:
-        " Admin interface page for admin to add, delete, edit posts as well as manage users ",
+        " Admin Blog Site Connect with Backend Blogs ,React Hook , Ckeditor, Axios, moment ,Hook Form ,Redux, Toastify , Bootstrap",
       detail: {
         description: "Interface page helps admin manage posts and users",
         Frontend: "React",
@@ -174,24 +171,8 @@ const Project = () => {
         github: "https://github.com/omodtung/admin_site_blog_fe.git",
       },
     },
-    
 
-    {
-      image: [
-        <FaReact size={50} color={"#7BEDFA"} />,
-      ],
-      title: "Client blog Site",
-      shortDescription:
-        " front-end page that doesn't require a login for users to interact with posts ... ",
-      detail: {
-        description: "front-end page for users to interact with and read posts",
-        Frontend: "Next.js",
-        member: 1,
-        role: "Frontend Dev",
-        demo: "", //link video Or github or Deploy
-        github: "https://github.com/omodtung/Client_site_Blog.git",
-      },
-    },
+   
     {
       image: [
         <FaReact size={50} color={"#7BEDFA"} />,
@@ -199,26 +180,56 @@ const Project = () => {
       ],
       title: "Porfolio Site",
       shortDescription:
-        " front-end page that doesn't require a login for users to interact with posts ... ",
+        " Personal project introduction page  ",
       detail: {
-        description: "front-end page for users to interact with and read posts",
-        Frontend: "Next.js",
+        description: "Porfolio Site With React",
+        Frontend: "React",
         member: 1,
         role: "Frontend Dev",
         demo: "", //link video Or github or Deploy
-        github: "https://github.com/omodtung/Client_site_Blog.git",
+        github: "https://github.com/omodtung/Portfolio-react.git",
       },
     },
     {
       image: [
-        <FaJava  size={50} color={"#c9c938"}/>
+        <FaJava size={50} color={"#c9c938"} />,
         // <FaCss3 size={50} color={"#ff1100"} />,
       ],
+      title: "Desktop App-Member Management-Desktop App",
+      shortDescription:
+        " Member management, Equipment, and Violation Handling ,Static ",
+      detail: {
+        description: "  MEMBER MANAGEMENT - DESKTOP APP using MySql , JavaSwing ,JDBC ,Xampp",
+       database :"Mysql",
+        member: 9,
+        role: "Java Dev",
+        demo: "", //link video Or github or Deploy
+        github: "https://github.com/omodtung/MHPL.git",
+      },
+    },
+    {
+      image: <SiHibernate size={50} color={"#40ff00"} />,
+      title: " MEMBER MANAGEMENT - DESKTOP APP ",
+      shortDescription:
+        " Member management, Equipment, and Violation Handling ,Static",
+      detail: {
+        description:
+          " MEMBER MANAGEMENT - DESKTOP APP using Hibernate , MySql , JavaSwing ,JDBC ,Xampp",
+
+        
+        database: "MysqL",
+        member: 9,
+        role: "Java Dev",
+        github: "https://github.com/omodtung/MHPL_QLTV.git", //link video Or github or Deploy
+      },
+    },
+    {
+      image: [<FaReact size={50} color={"#7BEDFA"} />],
       title: "Client blog Site",
       shortDescription:
-        " front-end page that doesn't require a login for users to interact with posts ... ",
+        " Client blog Site NextJs ,Boostrap ,moment",
       detail: {
-        description: "front-end page for users to interact with and read posts",
+        description: "Client blog Site Just Read Post ",
         Frontend: "Next.js",
         member: 1,
         role: "Frontend Dev",
@@ -232,22 +243,24 @@ const Project = () => {
       image: [
         <FaHtml5 size={50} color={"#ff1100"} />,
         <FaCss3 size={50} color={"#2225d6"} />,
-        <FaJs size={50} color = {"#d1d43b"}/>  ,
-        <FaBootstrap   size={50} color={"#b51fb5"}/>
+        <FaJs size={50} color={"#d1d43b"} />,
+        <FaBootstrap size={50} color={"#b51fb5"} />,
       ],
 
-      title: "Client blog Site",
+      title: "BLog Study Abroad Information",
       shortDescription:
-        " front-end page that doesn't require a login for users to interact with posts ... ",
+        " Study Abroad Information Using HTML , CSS ,JS ,Bootstrap ",
       detail: {
-        description: "front-end page for users to interact with and read posts",
-        Frontend: "Next.js",
-        member: 1,
+        description: "Study Abroad Information Using HTML , CSS ,JS ,Bootstrap",
+        Frontend: "HTML , CSS ,JS ,Bootstrap",
+        member: 4,
         role: "Frontend Dev",
         demo: "", //link video Or github or Deploy
-        github: "https://github.com/omodtung/Client_site_Blog.git",
+        github: "https://github.com/omodtung/Blog_Thong_Tin_Du_Hoc.git",
       },
-    },
+    }, 
+   
+    
     
   ];
   return (
@@ -303,7 +316,8 @@ const Project = () => {
               <h3>Amazing Services</h3>
               <span>Meet our amazing services</span>
             </div>
-            <div className="list_wrap">
+            <div className="list_wrap" style={{    flex: '1',padding: '10px', display: 'flex'
+ }} >
               <ul>
                 {DataProject.map((item, index) => {
                   return (
